36f5e9f8772fede5a6897c360113487efcc4e7ec
Griotold <gotjd9773@naver.com>
2025-05-12 12:38:59 +0900

    Initial commit

---
+++ src/main/java/com/sparta/backend_12/Backend12Application.java
@@ -0,0 +1,13 @@
+package com.sparta.backend_12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Backend12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Backend12Application.class, args);
+	}
+
+}
